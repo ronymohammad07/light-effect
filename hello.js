d67f57b1946ed85cc2b331fdfcaf755403e7cd54
ronymohammad07 <mdronemiah07@gmail.com>
2023-10-31 22:19:42 +0600

    added light effect

---
+++ hello.js
@@ -0,0 +1,24 @@
+// My First JavaScript Code
+var x, y ;
+x = 10;
+y = 30;
+a = x%y;
+console.log(a);
+// example usig var
+var myName = 'my name is rony.';
+var yourName = 'your name?';
+var pulas = myName + yourName ;
+console.log(pulas);
+
+var x, y;
+x = 39;
+y = 49;
+a = x+y;
+console.log(a)
+b = a - 39;
+console.log(b)
+
+var firatName = 'Rony';
+firstName = 'Miah';
+firstName = 'Rony Miah';
+console.log(firstName)
